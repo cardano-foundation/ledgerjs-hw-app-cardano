0e27bb8f8ea651489295002e759462977da5de8b
Peter Peresini <peter.peresini@gmail.com>
2019-03-22 13:17:33 +0100

    Update copyright

--- src/Ada.js
+++ src/Ada.js
@@ -1,6 +1,7 @@
 /********************************************************************************
  *   Ledger Node JS API
  *   (c) 2016-2017 Ledger
+ *   (c) 2019 VacuumLabs
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
