610844fc9cbaa4dd65f1143ef60f2821fb800a3e
Rafael Korbas <rafael.korbas@gmail.com>
2020-07-08 21:42:06 +0200

    Proof of concept of signing tx from js

--- test/src/integration/signTx.js
+++ test/src/integration/signTx.js
@@ -3,14 +3,7 @@ import { getAda, str_to_path } from "../test_utils";
 
 const inputs = {
   utxo0: {
-    txDataHex:
-      "839f8200d8185824825820918c11e1c041a0cb04baea651b9fb1bdef7ee5295f" +
-      "032307e2e57d109de118b8008200d81858248258208f34e4f719effe82c28c8f" +
-      "f45e426233651fc03686130cb7e1d4bc6de20e689c01ff9f8282d81858218358" +
-      "1cb6f4b193e083530aca83ff03de4a60f4e7a6732b68b4fa6972f42c11a0001a" +
-      "907ab5c71a000f42408282d818584283581cb5bacd405a2dcedce19899f8647a" +
-      "8c4f45d84c06fb532c63f9479a40a101581e581c6b8487e9d22850b7539db255" +
-      "e27dd48dc0a50c7994d678696be64f21001ac5000d871a03dc396fffa0",
+    txHashHex: "01f54c866c778568c01b9e4c0a2cbab29e0af285623404e0ef922c6b63f9b222",
     outputIndex: 0,
     path: str_to_path("44'/1815'/0'/0/0")
   }
@@ -60,7 +53,9 @@ describe("signTx", async () => {
   it("Should correctly sign Tx", async () => {
     const response = await ada.signTransaction(
       [inputs.utxo0],
-      [outputs.adalite, outputs.ledgerChange]
+      [outputs.adalite, outputs.ledgerChange],
+      "0",
+      "0",
     );
     expect(response).to.deep.equal(results);
   });
