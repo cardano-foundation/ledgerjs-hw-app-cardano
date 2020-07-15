7705433816f0c2797409577dce44f3e18f24acb2
Rafael Korbas <rafael.korbas@gmail.com>
2020-07-15 14:15:10 +0200

    fix shelley derivation

--- test/src/integration/deriveAddress.js
+++ test/src/integration/deriveAddress.js
@@ -2,6 +2,7 @@ import { expect } from "chai";
 import pathDerivations from "./__fixtures__/pathDerivations";
 
 import { getAda, str_to_path, hex_to_buf } from "../test_utils";
+import { AddressTypeNibbles } from "../../../lib/Ada";
 
 const BYRON_PROTOCOL_MAGIC = 764824073;
 
@@ -61,27 +62,27 @@ describe("deriveAddress", async () => {
     };
 
     // base
-    await test(0x00, 0x03, "1852'/1815'/0'/0/1", "1852'/1815'/0'/2/0", null, null,
+    await test(AddressTypeNibbles.BYRON, 0x03, "1852'/1815'/0'/0/1", "1852'/1815'/0'/2/0", null, null,
         "addr1qdd9xypc9xnnstp2kas3r7mf7ylxn4sksfxxypvwgnc63vcayfawlf9hwv2fzuygt2km5v92kvf8e3s3mk7ynxw77cwqdquehe");
-    await test(0x00, 0x00, "1852'/1815'/0'/0/1", "1852'/1815'/0'/2/0", null, null,
+    await test(AddressTypeNibbles.BYRON, 0x00, "1852'/1815'/0'/0/1", "1852'/1815'/0'/2/0", null, null,
         "addr1qpd9xypc9xnnstp2kas3r7mf7ylxn4sksfxxypvwgnc63vcayfawlf9hwv2fzuygt2km5v92kvf8e3s3mk7ynxw77cwqhn8sgh");
-    await test(0x00, 0x00, "1852'/1815'/0'/0/1", null, "1d227aefa4b773149170885aadba30aab3127cc611ddbc4999def61c", null,
+    await test(AddressTypeNibbles.BYRON, 0x00, "1852'/1815'/0'/0/1", null, "1d227aefa4b773149170885aadba30aab3127cc611ddbc4999def61c", null,
         "addr1qpd9xypc9xnnstp2kas3r7mf7ylxn4sksfxxypvwgnc63vcayfawlf9hwv2fzuygt2km5v92kvf8e3s3mk7ynxw77cwqhn8sgh");
-    await test(0x00, 0x03, "1852'/1815'/0'/0/1", null, "122a946b9ad3d2ddf029d3a828f0468aece76895f15c9efbd69b4277", null,
+    await test(AddressTypeNibbles.BYRON, 0x03, "1852'/1815'/0'/0/1", null, "122a946b9ad3d2ddf029d3a828f0468aece76895f15c9efbd69b4277", null,
         "addr1qdd9xypc9xnnstp2kas3r7mf7ylxn4sksfxxypvwgnc63vcj922xhxkn6twlq2wn4q50q352annk3903tj00h45mgfmswz93l5");
 
     // enterprise
-    await test(0x60, 0x00, "1852'/1815'/0'/0/1", null, null, null,
+    await test(AddressTypeNibbles.ENTERPRISE, 0x00, "1852'/1815'/0'/0/1", null, null, null,
         "addr1vpd9xypc9xnnstp2kas3r7mf7ylxn4sksfxxypvwgnc63vc93wyej");
-    await test(0x60, 0x03, "1852'/1815'/0'/0/1", null, null, null,
+    await test(AddressTypeNibbles.ENTERPRISE, 0x03, "1852'/1815'/0'/0/1", null, null, null,
         "addr1vdd9xypc9xnnstp2kas3r7mf7ylxn4sksfxxypvwgnc63vc9wh7em");
 
     // pointer
-    await test(0x40, 0x00, "1852'/1815'/0'/0/1", null, null, [1, 2, 3],
+    await test(AddressTypeNibbles.POINTER, 0x00, "1852'/1815'/0'/0/1", null, null, [1, 2, 3],
         "addr1gpd9xypc9xnnstp2kas3r7mf7ylxn4sksfxxypvwgnc63vcpqgpsh506pr");
-    await test(0x40, 0x03, "1852'/1815'/0'/0/1", null, null, [24157, 177, 42],
+    await test(AddressTypeNibbles.POINTER, 0x03, "1852'/1815'/0'/0/1", null, null, [24157, 177, 42],
         "addr1gdd9xypc9xnnstp2kas3r7mf7ylxn4sksfxxypvwgnc63vuph3wczvf288aeyu");
-    await test(0x40, 0x03, "1852'/1815'/0'/0/1", null, null, [0, 0, 0],
+    await test(AddressTypeNibbles.POINTER, 0x03, "1852'/1815'/0'/0/1", null, null, [0, 0, 0],
         "addr1gdd9xypc9xnnstp2kas3r7mf7ylxn4sksfxxypvwgnc63vcqqqqqnnd32q");
 
         // TODO add more, reward addresses at least
@@ -91,7 +92,7 @@ describe("deriveAddress", async () => {
     const test = async path => {
       const SHOULD_HAVE_THROWN = "should have thrown earlier";
       try {
-        await ada.deriveAddress(0b1000, BYRON_PROTOCOL_MAGIC, str_to_path(path));
+        await ada.deriveAddress(AddressTypeNibbles.BYRON, BYRON_PROTOCOL_MAGIC, str_to_path(path));
 
         throw new Error(SHOULD_HAVE_THROWN);
       } catch (error) {
