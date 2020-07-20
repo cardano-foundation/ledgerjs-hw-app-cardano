4aa3df382099d913fff2577879f404d7204e5784
Jan Mazak <jan.mazak@gmail.com>
2020-07-20 15:41:57 +0200

    fix test answers

--- test/src/integration/getVersion.js
+++ test/src/integration/getVersion.js
@@ -14,7 +14,8 @@ describe("getVersion", async () => {
 
   it("Should correctly get the semantic version of device", async () => {
     const response = await ada.getVersion();
-    expect(response.major).to.equal(1);
-    expect(response.minor).to.equal(1);
+
+    expect(response.major).to.equal(2);
+    expect(response.minor).to.equal(0);
   });
 });
