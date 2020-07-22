3f189fbc7814de08d411f9784c4d2cc2959d9037
Jan Mazak <jan.mazak@gmail.com>
2020-07-22 12:09:35 +0200

    finish sign tx tests

--- test/src/integration/signTx.js
+++ test/src/integration/signTx.js
@@ -112,62 +112,76 @@ const results = {
     ]
   },
   noChangeByronDaedalusMainnet: {
-    txHashHex: "73e09bdebf98a9e0f17f86a2d11e0f14f4f8dae77cdf26ff1678e821f20c8db6",
+    txHashHex: "3cf35b4d9bfa87b8eab5de659e0520bdac37b0de0b3840c1d8abd683330a9756",
     witnesses: [
       {
         path: str_to_path("44'/1815'/0'/0/0"),
         witnessSignatureHex:
-          "248e6f443503febbaa46a71f2007b024bc6d64f4ecd8897c4827b39ca45802377ed19d" +
-          "84a0565d50e681414748a884503a1aee224207878b0062aa179eeb4400"
+          "fdca7969a3e8bc091c9ee32c04732f79bb7c0091f1796fd2c0e1de8aa8547a00457d50d0576f4dd421baf754499cf0e77584e848e3547addd5d5b7167597a307"
       }
     ]
   },
   noChangeByronTestnet: {
-    txHashHex: "73e09bdebf98a9e0f17f86a2d11e0f14f4f8dae77cdf26ff1678e821f20c8db6",
+    txHashHex: "e2319ee8317ac537af4c2c3322aaf9fb6c64a95e3921ad75ab91b4f5b5306963",
     witnesses: [
       {
         path: str_to_path("44'/1815'/0'/0/0"),
         witnessSignatureHex:
-          "248e6f443503febbaa46a71f2007b024bc6d64f4ecd8897c4827b39ca45802377ed19d" +
-          "84a0565d50e681414748a884503a1aee224207878b0062aa179eeb4400"
+          "224d103185f4709f7b749339ff7ba432d50ca5cb742678847f5e574858cf7dda7ed402399a9ddba81ecd731b6f939ba07a247cd570dcd543f83a9aeadc4f9603"
       }
     ]
   },
   noChangeShelley: {
     /*
-    * txBody: a400818258201af8fa0b754ff99253d983894e63a2b09cbb56c833ba18c3384210163
-    * f63dcfc000181825841007cb05fce110fb999f01abb4f62bc455e217d4a51fde909fa9aea5454
-    * 43ac53c046cf6a42095e3c60310fa802771d0672f8fe2d1861138b09da61d425f346111401021
-    * 82a030a
+    * txBody: a400818258201af8fa0b754ff99253d983894e63a2b09cbb56c833ba18c3384210163f63dcfc000181825841017cb05fce110fb999f01abb4f62bc455e217d4a51fde909fa9aea545443ac53c046cf6a42095e3c60310fa802771d0672f8fe2d1861138b09da61d425f34611140102182a030a
     */
-    txHashHex: "73e09bdebf98a9e0f17f86a2d11e0f14f4f8dae77cdf26ff1678e821f20c8db6",
+    txHashHex: "97e40841b9b494d4c63993c35038484a72e70f0d5356471e9f1e3e311663b67e",
     witnesses: [
       {
-        path: str_to_path("44'/1815'/0'/0/0"),
+        path: str_to_path("1852'/1815'/0'/0/0"),
         witnessSignatureHex:
-          "248e6f443503febbaa46a71f2007b024bc6d64f4ecd8897c4827b39ca45802377ed19d" +
-          "84a0565d50e681414748a884503a1aee224207878b0062aa179eeb4400"
+          "5137e748df308525b46deaef18e61e04994f6658c32ada286b06d32b99d6676aef94f36895a51b9025d94b2ab0776749eedc7451ac1f0e61bef8b9cf5ec0240e"
       }
     ]
   },
   changeBaseWithStakingPath: {
-    txHashHex: "TODO",
+    txHashHex: "bd9e06485299c3c2be83135438f18fa4bde6e324420ae0a79dd2a12295f28597",
     witnesses: [
+      {
+        path: str_to_path("1852'/1815'/0'/0/0"),
+        witnessSignatureHex:
+          "ce028294627e27e8d80c5902f7dae91da851bf530df8b12efef44f7afa316b613cdb2b404e39df2a47f7fa64818a66b2b34d4b45ca3a04bf519c24c7a4ac3f00"
+      }
     ]
   },
   changeBaseWithStakingKeyHash: {
-    txHashHex: "TODO",
+    txHashHex: "f475a32afbf7b028fb794f81311a10f655afbbdf1d0201e5c801010a8cde9ea7",
     witnesses: [
+      {
+        path: str_to_path("1852'/1815'/0'/0/0"),
+        witnessSignatureHex:
+          "4ac5017c014886406a38a45417a165156280be63ca6975a5acffcabc0cc842ca603248b8a7ebfa729d7affce34518f4ca94fe797420a4d7aa0ef8c2b0ddfba0b"
+      }
     ]
   },
   changePointer: {
-    txHashHex: "TODO",
+    txHashHex: "4b19e27ffc006ace16592311c4d2f0cafc255eaa47a6178ff540c0a46d07027c",
     witnesses: [
+      {
+        path: str_to_path("1852'/1815'/0'/0/0"),
+        witnessSignatureHex:
+          "1838884e08cf6966ebe6b3e775191c4f08d90834723421779efd6aa96e52ffc91a24e5073abe6db94c74fe080d008258b3d989c159d9b87a9c778a51404abc08"
+      }
     ]
   },
   changeEnterprise: {
-    txHashHex: "TODO",
+    txHashHex: "c192b24a87d45c768f7f33ed37998054db96d34558e59afebabe51cfb7034b65",
     witnesses: [
+      {
+        path: str_to_path("1852'/1815'/0'/0/0"),
+        witnessSignatureHex:
+          "70559415746a9646dc492b7758e18cb367c005ab0479558b3d540be2310eb1bb1dd0839081e22c0b4727e8bd8e163cfbfe9def99a8506fb4a6787a200862e00f"
+      }
     ]
   },
   withWithdrawal: {
@@ -177,8 +191,13 @@ const results = {
     * e1a5d89d92f45fa0001a0d0c25611a002dd2e802182a030a05a1581c1d227aefa4b7731491708
     * 85aadba30aab3127cc611ddbc4999def61c186f
     */
-    txHashHex: "TODO",
+    txHashHex: "6fdce03a03f98c201f0889f695a7abc2305acc14a6638b7928fa182e9cec877f",
     witnesses: [
+      {
+        path: str_to_path("1852'/1815'/0'/0/0"),
+        witnessSignatureHex:
+          "d0f4bf6940ba4ab4d6ce99234afdfa974b8e3ecad209b699a2eda9d21087ee8a193e45e3ed5ec998d824bbc00eb1061dcf65184ae2b234485177f0c942150005"
+      }
     ]
   },
   withRegistrationCertificate: {
@@ -188,8 +207,13 @@ const results = {
     * e1a5d89d92f45fa0001a0d0c25611a002dd2e802182a030a048182008200581c1d227aefa4b77
     * 3149170885aadba30aab3127cc611ddbc4999def61c
     */
-    txHashHex: "TODO",
+    txHashHex: "a119ec712822b2f4bc96737121f286cf149ac2f8de2230e0d675f074094d1cd6",
     witnesses: [
+      {
+        path: str_to_path("1852'/1815'/0'/0/0"),
+        witnessSignatureHex:
+          "9825594e5a91333b9f5762665ba316af34c2208bd7ef073178af5e48f2aae8673d50436045e292d5bb9be7492eeeda475a04e58621a326c91049a2ef26a33200"
+      }
     ]
   },
   withDelegationCertificate: {
@@ -200,8 +224,13 @@ const results = {
     * 3149170885aadba30aab3127cc611ddbc4999def61c581cf61c42cbf7c8c53af3f520508212ad
     * 3e72f674f957fe23ff0acb4973
     */
-    txHashHex: "TODO",
+    txHashHex: "7afd028b504c3668102b129b37a86c09a2872f76741dc7a68e2149c808b3a8aa",
     witnesses: [
+      {
+        path: str_to_path("1852'/1815'/0'/0/0"),
+        witnessSignatureHex:
+          "d94c8f8fe73946c25f3bd0919d05a60b8373ef0a7261fa73eefe1f2a20e8a4c3401feb5eea701222184fceab2c45b47bd823ac76123e2d17f804d3e4ed2df909"
+      }
     ]
   },
   withDeregistrationCertificate: {
@@ -211,8 +240,13 @@ const results = {
     * e1a5d89d92f45fa0001a0d0c25611a002dd2e802182a030a048182018200581c1d227aefa4b77
     * 3149170885aadba30aab3127cc611ddbc4999def61c
     */
-    txHashHex: "TODO",
+    txHashHex: "8b143fae3b37748fee1decdc10fbfa554158b58fbc99623ecdd2ba7aa709e471",
     witnesses: [
+      {
+        path: str_to_path("1852'/1815'/0'/0/0"),
+        witnessSignatureHex:
+          "6136510eb91449474f6137c8d1c7c69eb518e3844a3e63a626be8cf4af91afa24e12f4fa578398bf0e7992e22dcfc5f9773fb8546b88c19e3abfdaa3bbe7a304"
+      }
     ]
   },
   withMetadata: {
@@ -222,8 +256,13 @@ const results = {
     * e1a5d89d92f45fa0001a0d0c25611a002dd2e802182a030a075820deadbeefdeadbeefdeadbee
     * fdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef
     */
-    txHashHex: "TODO",
+    txHashHex: "34c1dd59c14252008b680bf6a727c8f371e2d96e8bca6b783bcf3f8f36407e6f",
     witnesses: [
+      {
+        path: str_to_path("1852'/1815'/0'/0/0"),
+        witnessSignatureHex:
+          "953c5243ba09570dd4e52642236834c138ad4abbbb21796a90540a11e8dc96e47043401d370cdaed70ebc332dd4db80c9b167fd7f20971c4f142875cea57200c"
+      }
     ]
   }
 };
