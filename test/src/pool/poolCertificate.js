00a420c0aabe23459d0eb98cfb07ecc9e8be255a
xdzurman <kamil.dzurman@gmail.com>
2020-10-08 14:11:48 +0200

    Tweak test values, supply tx hex

--- test/src/pool/poolCertificate.js
+++ test/src/pool/poolCertificate.js
@@ -49,7 +49,7 @@ const poolMetadataVariations = {
 
 const stakingHashOwners = {
   owner0: {
-    stakingKeyHashHex: "fc38b160e62718716922612711c277ae8b363977dcfb020c4189fba7"
+    stakingKeyHashHex: "794d9b3408c9fb67b950a48a0690f070f117e9978f7fc1d120fc58ad"
   },
   owner1: {
     stakingKeyHashHex: "0bd5d796f5e54866a14300ec2a18d706f7461b8f0502cc2a182bc88d"
@@ -58,7 +58,7 @@ const stakingHashOwners = {
 
 const stakingPathOwners = {
   owner0: {
-    stakingPath: str_to_path("1852'/1815'/0'/2/0")
+    stakingPath: str_to_path("1852'/1815'/0'/2/0") // hash: 1d227aefa4b773149170885aadba30aab3127cc611ddbc4999def61c
   },
   owner1: {
     stakingPath: str_to_path("1852'/1815'/0'/2/1")
@@ -120,8 +120,8 @@ const relays = {
     type: 0,
     params: {
       portNumber: 3000,
-      ipv4Hex: "36e44b9a",
-      ipv6Hex: "24ff780133a2e383a5c4340a07c276e5"
+      ipv4Hex: "36e44b9b",
+      ipv6Hex: "0178ff2483e3a2330a34c4a5e576c207"
     }
   },
   singleHostNameRelay: {
@@ -148,7 +148,7 @@ const relays = {
   multiHostNameRelay: {
     type: 2,
     params: {
-      dnsName: "aaaa.bbbb.com"
+      dnsName: "aaaa.bbbc.com"
     }
   },
   multiHostNameRelayMissingDns: {
@@ -167,21 +167,27 @@ const relayVariationSet = {
   multiHostNameRelay: [relays.multiHostNameRelay], // reportedly not implemented
   twoIPV4Relays: [relays.singleHostIPV4Relay0, relays.singleHostIPV4Relay1],
   combinedIPV4SingleHostNameRelays: [relays.singleHostIPV4Relay0, relays.singleHostNameRelay],
-  combinedIPV4IPV6Relays: [relays.singleHostIPV4Relay1, relays.singleHostIPV6Relay]
+  combinedIPV4IPV6Relays: [relays.singleHostIPV4Relay1, relays.singleHostIPV6Relay],
+  allRelays: [
+    relays.singleHostIPV4Relay0,
+    relays.singleHostIPV6Relay,
+    relays.singleHostNameRelay,
+    relays.multiHostNameRelay
+  ]
 }
 
 const defaultPoolRegistration = {
   type: 3,
   poolRegistrationParams: {
-    poolKeyHashHex: "7d7ef6b9789d2e56f40d64fd68a0978263b78819c85d806517ba8531",
-    vrfKeyHashHex: "d318306e883533656c137a4b97101c011add371c6cb3f26de7c0084ebd2d08ac",
+    poolKeyHashHex: "13381d918ec0283ceeff60f7f4fc21e1540e053ccf8a77307a7a32ad",
+    vrfKeyHashHex: "07821cd344d7fd7e3ae5f2ed863218cb979ff1d59e50c4276bdc479b0d084450",
     pledgeStr: "50000000000",
     costStr: "340000000",
     margin: {
       numerator: "3",
       denominaror: "100",
     },
-    rewardAccountKeyHash: "e0fc38b160e62718716922612711c277ae8b363977dcfb020c4189fba7",
+    rewardAccountKeyHash: "e1794d9b3408c9fb67b950a48a0690f070f117e9978f7fc1d120fc58ad",
     poolOwners: poolOwnerVariationSet.singlePathOwner,
     relays: relayVariationSet.singleHostIPV4Relay,
     metadata: poolMetadataVariations.poolMetadataDefault
@@ -210,6 +216,14 @@ const certificates = {
     poolRegistrationParams: {
       ...defaultPoolRegistration.poolRegistrationParams,
       poolOwners: poolOwnerVariationSet.twoCombinedOwners
+    }
+  },
+  poolRegistrationMixedOwnersAllRelays: {
+    ...defaultPoolRegistration,
+    poolRegistrationParams: {
+      ...defaultPoolRegistration.poolRegistrationParams,
+      poolOwners: poolOwnerVariationSet.twoCombinedOwners,
+      relays: relayVariationSet.allRelays
     }
   },
   poolRegistration2PathOwners: {
@@ -349,6 +363,37 @@ describe("witnessCertificate", async () => {
 
   it("Should correctly witness valid multiple mixed owners mixed ipv4 ipv6 relays pool registration", async () => {
     const cert = certificates.poolRegistrationMixedOwnersIpv4Ipv6Relays
+    const response = await ada.signTransaction(
+      NetworkIds.MAINNET,
+      ProtocolMagics.MAINNET,
+      [inputs.utxo],
+      [
+        outputs.external,
+      ],
+      sampleFeeStr,
+      sampleTtlStr,
+      [cert],
+      [],
+      null
+    );
+    // expect(response).to.deep.equal();
+  });
+
+  it("Should correctly witness valid multiple mixed owners all relays pool registration", async () => {
+    /*
+    * txBody: 82a500818258203b40265111d8bb3c3c608d95b3a0bf83461ace32d79336579a1939b3aad1c0b7
+    * 000181825839017cb05fce110fb999f01abb4f62bc455e217d4a51fde909fa9aea545443ac53c046cf6a4209
+    * 5e3c60310fa802771d0672f8fe2d1861138b090102182a030a04818a03581c13381d918ec0283ceeff60f7f4
+    * fc21e1540e053ccf8a77307a7a32ad582007821cd344d7fd7e3ae5f2ed863218cb979ff1d59e50c4276bdc47
+    * 9b0d0844501b0000000ba43b74001a1443fd00d81e82031864581de1794d9b3408c9fb67b950a48a0690f070
+    * f117e9978f7fc1d120fc58ad82581c1d227aefa4b773149170885aadba30aab3127cc611ddbc4999def61c58
+    * 1c794d9b3408c9fb67b950a48a0690f070f117e9978f7fc1d120fc58ad848400190bb84436e44b9af6840019
+    * 0bb84436e44b9b500178ff2483e3a2330a34c4a5e576c2078301190bb86d616161612e626262622e636f6d82
+    * 026d616161612e626262632e636f6d82782968747470733a2f2f7777772e76616375756d6c6162732e636f6d
+    * 2f73616d706c6555726c2e6a736f6e5820cdb714fd722c24aeb10c93dbb0ff03bd4783441cd5ba2a8b6f3733
+    * 90520535bbf6
+    */
+    const cert = certificates.poolRegistrationMixedOwnersAllRelays
     const response = await ada.signTransaction(
       NetworkIds.MAINNET,
       ProtocolMagics.MAINNET,
