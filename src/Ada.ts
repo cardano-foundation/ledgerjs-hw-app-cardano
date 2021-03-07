c25ac86456182e1d48406b8ed75e547cce338b47
Peter Peresini <peter.peresini@gmail.com>
2021-03-07 07:35:51 +0100

    Move wrapRetryStillInCall

--- src/Ada.ts
+++ src/Ada.ts
@@ -19,8 +19,6 @@
 import type { BIP32Path, Certificate, DeriveAddressResponse, GetExtendedPublicKeyResponse, GetSerialResponse, GetVersionResponse, InputTypeUTxO, SignTransactionResponse, StakingBlockchainPointer, TxOutput, Withdrawal } from 'types/public';
 
 import cardano from './cardano'
-import type { INS } from "./interactions/common/ins";
-import { wrapRetryStillInCall } from './interactions/common/retry';
 import type { Interaction, SendParams } from './interactions/common/types';
 import { deriveAddress } from "./interactions/deriveAddress";
 import { getExtendedPublicKeys } from "./interactions/getExtendedPublicKeys";
@@ -120,6 +118,34 @@ function wrapConvertError<T extends Function>(fn: T): T {
 export type SendFn = (params: SendParams) => Promise<Buffer>;
 
 export type Transport = any
+
+// It can happen that we try to send a message to the device
+// when the device thinks it is still in a middle of previous ADPU stream.
+// This happens mostly if host does abort communication for some reason
+// leaving ledger mid-call.
+// In this case Ledger will respond by ERR_STILL_IN_CALL *and* resetting its state to
+// default. We can therefore transparently retry the request.
+
+// Note though that only the *first* request in an multi-APDU exchange should be retried.
+function wrapRetryStillInCall<T extends Function>(fn: T): T {
+  // @ts-ignore
+  return async (...args: any) => {
+    try {
+      return await fn(...args);
+    } catch (e) {
+      if (
+        e &&
+        e.statusCode &&
+        e.statusCode === DeviceErrorCodes.ERR_STILL_IN_CALL
+      ) {
+        // Do the retry
+        return await fn(...args);
+      }
+      throw e;
+    }
+  };
+}
+
 
 async function interact<T>(
   _send: SendFn,
