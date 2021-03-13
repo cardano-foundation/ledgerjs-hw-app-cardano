77da99bd33aab7e1dbedd98b8b1688299cf9f68d
Peter Peresini <peter.peresini@gmail.com>
2021-03-13 16:46:30 +0100

    Cleanup derive address tests

--- test/integration/deriveAddress.test.ts
+++ test/integration/deriveAddress.test.ts
@@ -2,10 +2,9 @@ import chai, { expect } from "chai";
 import chaiAsPromised from "chai-as-promised"
 
 import type { Ada } from "../../src/Ada";
-import { AddressType, DeviceStatusError, utils } from "../../src/Ada";
-import { str_to_path } from "../../src/utils/address";
-import { getAda, Networks, } from "../test_utils";
-import { byronTestcases, shelleyTestcases } from "./__fixtures__/deriveAddress";
+import { utils } from "../../src/Ada";
+import { getAda } from "../test_utils";
+import { byronTestcases, InvalidPathTestcases, shelleyTestcases } from "./__fixtures__/deriveAddress";
 
 chai.use(chaiAsPromised)
 
@@ -24,51 +23,38 @@ describe("deriveAddress", async () => {
   });
 
   describe("Should succesfully derive Byron address", async () => {
-    for (const { network, path, address: expected } of byronTestcases) {
-      it(path, async () => {
+    for (const { testname, network, addressParams, result: expectedResult } of byronTestcases) {
+      it(testname, async () => {
         const { addressHex } = await ada.deriveAddress({
           network,
-          address: {
-            type: AddressType.BYRON,
-            params: {
-              spendingPath: str_to_path(path)
-            }
-          }
+          address: addressParams
         });
 
-        expect(address_hex_to_base58(addressHex)).to.equal(expected);
+        expect(address_hex_to_base58(addressHex)).to.equal(expectedResult);
       })
     }
   });
 
   describe("Should succesfully derive Shelley address", async () => {
-    for (const [testname, testcases] of Object.entries(shelleyTestcases)) {
+    for (const { testname, network, addressParams, result: expectedResult } of shelleyTestcases) {
       it(testname, async () => {
-        for (const [network, addressParams, expectedResult] of testcases) {
-          const { addressHex } = await ada.deriveAddress({ network, address: addressParams })
+        const { addressHex } = await ada.deriveAddress({ network, address: addressParams })
 
-          expect(utils.bech32_encodeAddress(utils.hex_to_buf(addressHex as any))).to.equal(
-            expectedResult
-          );
-        }
+        expect(utils.bech32_encodeAddress(utils.hex_to_buf(addressHex as any))).to.equal(
+          expectedResult
+        );
       })
     }
   }).timeout(60000);
 
   describe("Should not permit invalid path", async () => {
-    const invalidPaths = ["44'/1815'/1'", "44'/1815'/1'/5/10'/1/2/3"]
-    for (const path of invalidPaths) {
-      it(path, async () => {
+    for (const { testname, network, addressParams, errCls, errMsg } of InvalidPathTestcases) {
+      it(testname, async () => {
         const promise = ada.deriveAddress({
-          network: Networks.Fake,
-          address: {
-            type: AddressType.BYRON,
-            params: {
-              spendingPath: str_to_path(path)
-            }
-          }
+          network,
+          address: addressParams
         })
-        await expect(promise).to.be.rejectedWith(DeviceStatusError, "Action rejected by Ledger's security policy");
+        await expect(promise).to.be.rejectedWith(errCls, errMsg);
       })
     }
   });
