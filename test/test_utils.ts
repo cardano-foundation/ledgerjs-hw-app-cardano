f27f25f4abada33f4f4fcdf710fdf057e1a294fa
Jakub Arbet <hi@jakubarbet.me>
2021-08-12 16:58:06 +0200

    Add mechanism to turn off validation

--- test/test_utils.ts
+++ test/test_utils.ts
@@ -1,7 +1,9 @@
 // @ts-ignore
 import TransportNodeHid from "@ledgerhq/hw-transport-node-hid"
+import { ImportMock } from "ts-mock-imports"
 
 import Ada from "../src/Ada"
+import * as parseModule from "../src/utils/parse"
 
 export async function getTransport() {
     return await TransportNodeHid.create(1000)
@@ -13,6 +15,35 @@ export async function getAda() {
     const ada = new Ada(transport);
     (ada as any).t = transport
     return Promise.resolve(ada)
+}
+
+export function turnOffValidation() {
+    const validate_mock = ImportMock.mockFunction(parseModule, 'validate')
+
+    const fns = ['isString', 'isInteger', 'isArray', 'isBuffer', 'isHexString', 'isHexStringOfLength', 'isValidPath']
+    /* @ts-ignore */
+    const mocks = fns.map((fn) => ImportMock.mockFunction(parseModule, fn, true))
+
+    return () => {
+        validate_mock.restore()
+        mocks.forEach((mock) => mock.restore())
+    }
+}
+
+export function describeWithoutValidation(title: string, test: () => void) {
+    describe(title, async () => {
+        let restoreValidation: () => void
+
+        before(() => {
+            restoreValidation = turnOffValidation()
+        })
+
+        after(() => {
+            restoreValidation()
+        })
+
+        test()
+    })
 }
 
 const ProtocolMagics = {
