vacuumlabs/ledgerjs-cardano-shelley

feature: new format for governance voting (CIP-36)

src/interactions/serialization/governanceVotingRegistration.ts
@@ -1,0 +1,109 @@
+import type { GovernanceVotingPublicKey, ParsedGovernanceVotingDelegation, ParsedGovernanceVotingRegistrationParams, ParsedOutputDestination,Uint8_t,Uint32_t, Uint64_str,ValidBIP32Path } from "../../types/internal"
+import type { Version } from "../../types/public"
+import { GovernanceVotingDelegationType } from "../../types/public"
+import { GovernanceVotingRegistrationFormat } from "../../types/public"
+import { assert, unreachable } from "../../utils/assert"
+import { hex_to_buf, path_to_buf, serializeOptionFlag,uint8_to_buf,uint32_to_buf,uint64_to_buf } from "../../utils/serialize"
+import { serializeTxOutputDestination } from "./txOutput"
+
+export function serializeGovernanceVotingRegistrationInit(params: ParsedGovernanceVotingRegistrationParams): Buffer {
+    const governanceRegistrationFormatEncoding = {
+        [GovernanceVotingRegistrationFormat.CIP_15]: 0x01,
+        [GovernanceVotingRegistrationFormat.CIP_36]: 0x02,
+    } as const
+    const formatBuffer = uint8_to_buf(governanceRegistrationFormatEncoding[params.format] as Uint8_t)
+
+    const numDelegations = params.delegations != null
+        ? params.delegations.length
+        : 0
+    const numDelegationsBuffer = uint32_to_buf(numDelegations as Uint32_t)
+
+    return Buffer.concat([
+        formatBuffer, // 1 B
+        numDelegationsBuffer, // 4 B
+    ])
+}
+
+function delegationTypeBuffer(type: GovernanceVotingDelegationType): Buffer {
+    const delegationTypeEncoding = {
+        [GovernanceVotingDelegationType.KEY]: 0x01,
+        [GovernanceVotingDelegationType.PATH]: 0x02,
+    } as const
+    return uint8_to_buf(delegationTypeEncoding[type] as Uint8_t)
+}
+
+export function serializeGovernanceVotingRegistrationVotingKey(
+    votingPublicKey: GovernanceVotingPublicKey | null,
+    votingPublicKeyPath: ValidBIP32Path | null,
+): Buffer {
+    if (votingPublicKey != null) {
+        assert(votingPublicKeyPath == null, "redundant governance registration voting key path")
+        return Buffer.concat([
+            delegationTypeBuffer(GovernanceVotingDelegationType.KEY),
+            hex_to_buf(votingPublicKey),
+        ])
+    } else {
+        assert(votingPublicKeyPath != null, "missing governance registration voting key")
+        return Buffer.concat([
+            delegationTypeBuffer(GovernanceVotingDelegationType.PATH),
+            path_to_buf(votingPublicKeyPath!),
+        ])
+    }
+}
+
+export function serializeGovernanceVotingRegistrationDelegation(delegation: ParsedGovernanceVotingDelegation): Buffer {
+    const typeBuffer = delegationTypeBuffer(delegation.type)
+
+    const weightBuffer = uint32_to_buf(delegation.weight)
+
+    switch (delegation.type) {
+    case GovernanceVotingDelegationType.KEY:
+        return Buffer.concat([
+            typeBuffer,
+            hex_to_buf(delegation.votingPublicKey),
+            weightBuffer,
+        ])
+
+    case GovernanceVotingDelegationType.PATH:
+        return Buffer.concat([
+            typeBuffer,
+            path_to_buf(delegation.votingKeyPath),
+            weightBuffer,
+        ])
+
+    default:
+        unreachable(delegation)
+    }
+}
+
+export function serializeGovernanceVotingRegistrationStakingPath(stakingPath: ValidBIP32Path): Buffer {
+    return Buffer.concat([
+        path_to_buf(stakingPath),
+    ])
+}
+
+export function serializeGovernanceVotingRegistrationRewardsDestination(
+    rewardsDestination: ParsedOutputDestination,
+    version: Version,
+): Buffer {
+    return serializeTxOutputDestination(rewardsDestination, version)
+}
+
+export function serializeGovernanceVotingRegistrationNonce(nonce: Uint64_str): Buffer {
+    return Buffer.concat([
+        uint64_to_buf(nonce),
+    ])
+}
+
+export function serializeGovernanceVotingRegistrationVotingPurpose(votingPurpose: Uint64_str | null): Buffer {
+    const includeVotingPurposeBuffer = serializeOptionFlag(votingPurpose != null)
+
+    const votingPurposeBuffer = votingPurpose != null
+        ? uint64_to_buf(votingPurpose)
+        : Buffer.from([])
+
+    return Buffer.concat([
+        includeVotingPurposeBuffer, // 1 B
+        votingPurposeBuffer, // 0 or 8 B
+    ])
+}

src/parsing/txAuxiliaryData.ts
@@ -1,15 +1,17 @@
 import { InvalidData } from "../errors"
 import { InvalidDataReason } from "../errors/invalidDataReason"
-import type { ParsedCatalystRegistrationParams, ParsedTxAuxiliaryData, ShelleyAddressParams } from "../types/internal"
+import type { ParsedGovernanceVotingDelegation, ParsedGovernanceVotingRegistrationParams, ParsedTxAuxiliaryData } from "../types/internal"
+import { GOVERNANCE_VOTING_PUBLIC_KEY_LENGTH } from "../types/internal"
 import { AUXILIARY_DATA_HASH_LENGTH } from "../types/internal"
-import type { CatalystRegistrationParams,Network,TxAuxiliaryData } from "../types/public"
-import { AddressType } from "../types/public"
+import type { GovernanceVotingDelegation, GovernanceVotingRegistrationParams, Network,TxAuxiliaryData } from "../types/public"
+import { GovernanceVotingDelegationType, GovernanceVotingRegistrationFormat } from "../types/public"
 import { TxAuxiliaryDataType } from "../types/public"
-import { parseBIP32Path, parseHexStringOfLength, parseUint64_str } from "../utils/parse"
+import { unreachable } from "../utils/assert"
+import { isArray, parseBIP32Path, parseHexStringOfLength, parseUint32_t, parseUint64_str } from "../utils/parse"
 import { validate } from "../utils/parse"
-import { parseAddress } from "./address"
+import { parseTxDestination } from "./transaction"
 
-export const CATALYST_REGISTRATION_VOTING_VKEY_LENGTH = 32
+export const GOVERNANCE_VOTING_REGISTRATION_VOTING_VKEY_LENGTH = 32
 
 export function parseTxAuxiliaryData(network: Network, auxiliaryData: TxAuxiliaryData): ParsedTxAuxiliaryData {
     switch (auxiliaryData.type) {
@@ -19,10 +21,10 @@ export function parseTxAuxiliaryData(network: Network, auxiliaryData: TxAuxiliar
             hashHex: parseHexStringOfLength(auxiliaryData.params.hashHex, AUXILIARY_DATA_HASH_LENGTH, InvalidDataReason.AUXILIARY_DATA_INVALID_HASH),
         }
     }
-    case TxAuxiliaryDataType.CATALYST_REGISTRATION: {
+    case TxAuxiliaryDataType.GOVERNANCE_VOTING_REGISTRATION: {
         return {
-            type: TxAuxiliaryDataType.CATALYST_REGISTRATION,
-            params: parseCatalystRegistrationParams(network, auxiliaryData.params),
+            type: TxAuxiliaryDataType.GOVERNANCE_VOTING_REGISTRATION,
+            params: parseGovernanceVotingRegistrationParams(network, auxiliaryData.params),
         }
     }
     default:
@@ -30,14 +32,78 @@ export function parseTxAuxiliaryData(network: Network, auxiliaryData: TxAuxiliar
     }
 }
 
-function parseCatalystRegistrationParams(network: Network, params: CatalystRegistrationParams): ParsedCatalystRegistrationParams {
-    validate(params.rewardsDestination.type !== AddressType.BYRON, InvalidDataReason.CATALYST_REGISTRATION_INVALID_REWARDS_DESTINATION_BYRON)
+function parseGovernanceVotingDelegation(delegation: GovernanceVotingDelegation): ParsedGovernanceVotingDelegation {
+    const weight = parseUint32_t(delegation.weight, InvalidDataReason.GOVERNANCE_VOTING_DELEGATION_INVALID_WEIGHT)
+
+    switch(delegation.type) {
+    case GovernanceVotingDelegationType.KEY:
+        return {
+            type: delegation.type,
+            votingPublicKey: parseHexStringOfLength(delegation.votingPublicKeyHex, GOVERNANCE_VOTING_PUBLIC_KEY_LENGTH, InvalidDataReason.GOVERNANCE_VOTING_DELEGATION_INVALID_KEY),
+            weight,
+        }
+    case GovernanceVotingDelegationType.PATH:
+        return {
+            type: delegation.type,
+            votingKeyPath: parseBIP32Path(delegation.votingKeyPath, InvalidDataReason.GOVERNANCE_VOTING_DELEGATION_INVALID_PATH),
+            weight,
+        }
+    default:
+        throw new InvalidData(InvalidDataReason.GOVERNANCE_VOTING_DELEGATION_UNKNOWN_TYPE)
+    }
+}
+
+function parseGovernanceVotingDelegations(delegations: Array<GovernanceVotingDelegation>): Array<ParsedGovernanceVotingDelegation> {
+    validate(isArray(delegations), InvalidDataReason.GOVERNANCE_VOTING_REGISTRATION_DELEGATIONS_NOT_ARRAY)
+    return delegations.map(d => parseGovernanceVotingDelegation(d))
+}
+
+function parseGovernanceVotingRegistrationParams(network: Network, params: GovernanceVotingRegistrationParams): ParsedGovernanceVotingRegistrationParams {
+    switch (params.format) {
+    case GovernanceVotingRegistrationFormat.CIP_15:
+        validate(params.delegations == null, InvalidDataReason.GOVERNANCE_VOTING_REGISTRATION_INCONSISTENT_WITH_CIP15)
+        validate(params.votingPublicKeyHex != null, InvalidDataReason.GOVERNANCE_VOTING_REGISTRATION_INCONSISTENT_WITH_CIP15)
+        validate(params.votingPurpose == null, InvalidDataReason.GOVERNANCE_VOTING_REGISTRATION_INCONSISTENT_WITH_CIP15)
+        break
+
+    case GovernanceVotingRegistrationFormat.CIP_36:
+        // exactly one of delegations, votingPublicKeyHex, votingPublicKeyPath must be given
+        if (params.delegations != null) {
+            validate(params.votingPublicKeyHex == null && params.votingPublicKeyPath == null, InvalidDataReason.GOVERNANCE_VOTING_REGISTRATION_INCONSISTENT_WITH_CIP36)
+        } else {
+            validate(params.delegations == null, InvalidDataReason.GOVERNANCE_VOTING_REGISTRATION_INCONSISTENT_WITH_CIP36)
+            validate(params.votingPublicKeyHex == null || params.votingPublicKeyPath == null, InvalidDataReason.GOVERNANCE_VOTING_REGISTRATION_BOTH_KEY_AND_PATH)
+        }
+        break
+
+    default:
+        unreachable(params.format)
+    }
+
+    const votingPublicKey = params.votingPublicKeyHex == null
+        ? null
+        : parseHexStringOfLength(params.votingPublicKeyHex, GOVERNANCE_VOTING_REGISTRATION_VOTING_VKEY_LENGTH, InvalidDataReason.GOVERNANCE_VOTING_REGISTRATION_INVALID_VOTING_KEY)
+
+    const votingPublicKeyPath = params.votingPublicKeyPath == null
+        ? null
+        : parseBIP32Path(params.votingPublicKeyPath, InvalidDataReason.GOVERNANCE_VOTING_REGISTRATION_INVALID_VOTING_KEY_PATH)
+
+    const delegations = params.delegations == null
+        ? null
+        : parseGovernanceVotingDelegations(params.delegations)
+
+    const votingPurpose = params.votingPurpose == null
+        ? null
+        : parseUint64_str(params.votingPurpose, {}, InvalidDataReason.GOVERNANCE_VOTING_REGISTRATION_INVALID_VOTING_PURPOSE)
 
     return {
-        type: TxAuxiliaryDataType.CATALYST_REGISTRATION,
-        votingPublicKey: parseHexStringOfLength(params.votingPublicKeyHex, CATALYST_REGISTRATION_VOTING_VKEY_LENGTH, InvalidDataReason.CATALYST_REGISTRATION_INVALID_VOTING_KEY),
-        stakingPath: parseBIP32Path(params.stakingPath, InvalidDataReason.CATALYST_REGISTRATION_INVALID_STAKING_KEY_PATH),
-        rewardsDestination: parseAddress(network, params.rewardsDestination) as ShelleyAddressParams,
-        nonce: parseUint64_str(params.nonce, {}, InvalidDataReason.CATALYST_REGISTRATION_INVALID_NONCE),
+        format: params.format,
+        votingPublicKey,
+        votingPublicKeyPath,
+        delegations,
+        stakingPath: parseBIP32Path(params.stakingPath, InvalidDataReason.GOVERNANCE_VOTING_REGISTRATION_INVALID_STAKING_KEY_PATH),
+        rewardsDestination: parseTxDestination(network, params.rewardsDestination),
+        nonce: parseUint64_str(params.nonce, {}, InvalidDataReason.GOVERNANCE_VOTING_REGISTRATION_INVALID_NONCE),
+        votingPurpose,
     }
 }

cardano-foundation/ledgerjs-hw-app-cardano

src/interactions/serialization/governanceVotingRegistration.ts
@@ -1,0 +1,109 @@
+import type { GovernanceVotingPublicKey, ParsedGovernanceVotingDelegation, ParsedGovernanceVotingRegistrationParams, ParsedOutputDestination,Uint8_t,Uint32_t, Uint64_str,ValidBIP32Path } from "../../types/internal"
+import type { Version } from "../../types/public"
+import { GovernanceVotingDelegationType } from "../../types/public"
+import { GovernanceVotingRegistrationFormat } from "../../types/public"
+import { assert, unreachable } from "../../utils/assert"
+import { hex_to_buf, path_to_buf, serializeOptionFlag,uint8_to_buf,uint32_to_buf,uint64_to_buf } from "../../utils/serialize"
+import { serializeTxOutputDestination } from "./txOutput"
+
+export function serializeGovernanceVotingRegistrationInit(params: ParsedGovernanceVotingRegistrationParams): Buffer {
+    const governanceRegistrationFormatEncoding = {
+        [GovernanceVotingRegistrationFormat.CIP_15]: 0x01,
+        [GovernanceVotingRegistrationFormat.CIP_36]: 0x02,
+    } as const
+    const formatBuffer = uint8_to_buf(governanceRegistrationFormatEncoding[params.format] as Uint8_t)
+
+    const numDelegations = params.delegations != null
+        ? params.delegations.length
+        : 0
+    const numDelegationsBuffer = uint32_to_buf(numDelegations as Uint32_t)
+
+    return Buffer.concat([
+        formatBuffer, // 1 B
+        numDelegationsBuffer, // 4 B
+    ])
+}
+
+function serializeDelegationType(type: GovernanceVotingDelegationType): Buffer {
+    const delegationTypeEncoding = {
+        [GovernanceVotingDelegationType.KEY]: 0x01,
+        [GovernanceVotingDelegationType.PATH]: 0x02,
+    } as const
+    return uint8_to_buf(delegationTypeEncoding[type] as Uint8_t)
+}
+
+export function serializeGovernanceVotingRegistrationVotingKey(
+    votingPublicKey: GovernanceVotingPublicKey | null,
+    votingPublicKeyPath: ValidBIP32Path | null,
+): Buffer {
+    if (votingPublicKey != null) {
+        assert(votingPublicKeyPath == null, "redundant governance registration voting key path")
+        return Buffer.concat([
+            serializeDelegationType(GovernanceVotingDelegationType.KEY),
+            hex_to_buf(votingPublicKey),
+        ])
+    } else {
+        assert(votingPublicKeyPath != null, "missing governance registration voting key")
+        return Buffer.concat([
+            serializeDelegationType(GovernanceVotingDelegationType.PATH),
+            path_to_buf(votingPublicKeyPath),
+        ])
+    }
+}
+
+export function serializeGovernanceVotingRegistrationDelegation(delegation: ParsedGovernanceVotingDelegation): Buffer {
+    const typeBuffer = serializeDelegationType(delegation.type)
+
+    const weightBuffer = uint32_to_buf(delegation.weight)
+
+    switch (delegation.type) {
+    case GovernanceVotingDelegationType.KEY:
+        return Buffer.concat([
+            typeBuffer,
+            hex_to_buf(delegation.votingPublicKey),
+            weightBuffer,
+        ])
+
+    case GovernanceVotingDelegationType.PATH:
+        return Buffer.concat([
+            typeBuffer,
+            path_to_buf(delegation.votingKeyPath),
+            weightBuffer,
+        ])
+
+    default:
+        unreachable(delegation)
+    }
+}
+
+export function serializeGovernanceVotingRegistrationStakingPath(stakingPath: ValidBIP32Path): Buffer {
+    return Buffer.concat([
+        path_to_buf(stakingPath),
+    ])
+}
+
+export function serializeGovernanceVotingRegistrationRewardsDestination(
+    rewardsDestination: ParsedOutputDestination,
+    version: Version,
+): Buffer {
+    return serializeTxOutputDestination(rewardsDestination, version)
+}
+
+export function serializeGovernanceVotingRegistrationNonce(nonce: Uint64_str): Buffer {
+    return Buffer.concat([
+        uint64_to_buf(nonce),
+    ])
+}
+
+export function serializeGovernanceVotingRegistrationVotingPurpose(votingPurpose: Uint64_str | null): Buffer {
+    const includeVotingPurposeBuffer = serializeOptionFlag(votingPurpose != null)
+
+    const votingPurposeBuffer = votingPurpose != null
+        ? uint64_to_buf(votingPurpose)
+        : Buffer.from([])
+
+    return Buffer.concat([
+        includeVotingPurposeBuffer, // 1 B
+        votingPurposeBuffer, // 0 or 8 B
+    ])
+}

src/parsing/txAuxiliaryData.ts
@@ -1,15 +1,16 @@
 import { InvalidData } from "../errors"
 import { InvalidDataReason } from "../errors/invalidDataReason"
-import type { ParsedCatalystRegistrationParams, ParsedTxAuxiliaryData, ShelleyAddressParams } from "../types/internal"
+import type { ParsedGovernanceVotingDelegation, ParsedGovernanceVotingRegistrationParams, ParsedTxAuxiliaryData } from "../types/internal"
+import { GOVERNANCE_VOTING_PUBLIC_KEY_LENGTH } from "../types/internal"
 import { AUXILIARY_DATA_HASH_LENGTH } from "../types/internal"
-import type { CatalystRegistrationParams,Network,TxAuxiliaryData } from "../types/public"
-import { AddressType } from "../types/public"
+import type { GovernanceVotingDelegation, GovernanceVotingRegistrationParams, Network,TxAuxiliaryData } from "../types/public"
+import { GovernanceVotingDelegationType, GovernanceVotingRegistrationFormat } from "../types/public"
 import { TxAuxiliaryDataType } from "../types/public"
-import { parseBIP32Path, parseHexStringOfLength, parseUint64_str } from "../utils/parse"
+import { isArray, parseBIP32Path, parseHexStringOfLength, parseUint32_t, parseUint64_str } from "../utils/parse"
 import { validate } from "../utils/parse"
-import { parseAddress } from "./address"
+import { parseTxDestination } from "./transaction"
 
-export const CATALYST_REGISTRATION_VOTING_VKEY_LENGTH = 32
+export const GOVERNANCE_VOTING_REGISTRATION_VOTING_VKEY_LENGTH = 32
 
 export function parseTxAuxiliaryData(network: Network, auxiliaryData: TxAuxiliaryData): ParsedTxAuxiliaryData {
     switch (auxiliaryData.type) {
@@ -19,10 +20,10 @@ export function parseTxAuxiliaryData(network: Network, auxiliaryData: TxAuxiliar
             hashHex: parseHexStringOfLength(auxiliaryData.params.hashHex, AUXILIARY_DATA_HASH_LENGTH, InvalidDataReason.AUXILIARY_DATA_INVALID_HASH),
         }
     }
-    case TxAuxiliaryDataType.CATALYST_REGISTRATION: {
+    case TxAuxiliaryDataType.GOVERNANCE_VOTING_REGISTRATION: {
         return {
-            type: TxAuxiliaryDataType.CATALYST_REGISTRATION,
-            params: parseCatalystRegistrationParams(network, auxiliaryData.params),
+            type: TxAuxiliaryDataType.GOVERNANCE_VOTING_REGISTRATION,
+            params: parseGovernanceVotingRegistrationParams(network, auxiliaryData.params),
         }
     }
     default:
@@ -30,14 +31,79 @@ export function parseTxAuxiliaryData(network: Network, auxiliaryData: TxAuxiliar
     }
 }
 
-function parseCatalystRegistrationParams(network: Network, params: CatalystRegistrationParams): ParsedCatalystRegistrationParams {
-    validate(params.rewardsDestination.type !== AddressType.BYRON, InvalidDataReason.CATALYST_REGISTRATION_INVALID_REWARDS_DESTINATION_BYRON)
+function parseGovernanceVotingDelegation(delegation: GovernanceVotingDelegation): ParsedGovernanceVotingDelegation {
+    const weight = parseUint32_t(delegation.weight, InvalidDataReason.GOVERNANCE_VOTING_DELEGATION_INVALID_WEIGHT)
+
+    switch(delegation.type) {
+    case GovernanceVotingDelegationType.KEY:
+        return {
+            type: delegation.type,
+            votingPublicKey: parseHexStringOfLength(delegation.votingPublicKeyHex, GOVERNANCE_VOTING_PUBLIC_KEY_LENGTH, InvalidDataReason.GOVERNANCE_VOTING_DELEGATION_INVALID_KEY),
+            weight,
+        }
+    case GovernanceVotingDelegationType.PATH:
+        return {
+            type: delegation.type,
+            votingKeyPath: parseBIP32Path(delegation.votingKeyPath, InvalidDataReason.GOVERNANCE_VOTING_DELEGATION_INVALID_PATH),
+            weight,
+        }
+    default:
+        throw new InvalidData(InvalidDataReason.GOVERNANCE_VOTING_DELEGATION_UNKNOWN_DELEGATION_TYPE)
+    }
+}
+
+function parseGovernanceVotingDelegations(delegations: Array<GovernanceVotingDelegation>): Array<ParsedGovernanceVotingDelegation> {
+    validate(isArray(delegations), InvalidDataReason.GOVERNANCE_VOTING_REGISTRATION_DELEGATIONS_NOT_ARRAY)
+    return delegations.map(d => parseGovernanceVotingDelegation(d))
+}
+
+function parseGovernanceVotingRegistrationParams(network: Network, params: GovernanceVotingRegistrationParams): ParsedGovernanceVotingRegistrationParams {
+    switch (params.format) {
+    case GovernanceVotingRegistrationFormat.CIP_15:
+        validate(params.delegations == null, InvalidDataReason.GOVERNANCE_VOTING_REGISTRATION_INCONSISTENT_WITH_CIP15)
+        validate(params.votingPublicKeyHex != null, InvalidDataReason.GOVERNANCE_VOTING_REGISTRATION_INCONSISTENT_WITH_CIP15)
+        validate(params.votingPurpose == null, InvalidDataReason.GOVERNANCE_VOTING_REGISTRATION_INCONSISTENT_WITH_CIP15)
+        break
+
+    case GovernanceVotingRegistrationFormat.CIP_36:
+        // exactly one of delegations, votingPublicKeyHex, votingPublicKeyPath must be given
+        if (params.delegations != null) {
+            validate(params.votingPublicKeyHex == null && params.votingPublicKeyPath == null, InvalidDataReason.GOVERNANCE_VOTING_REGISTRATION_INCONSISTENT_WITH_CIP36)
+        } else {
+            validate(params.delegations == null, InvalidDataReason.GOVERNANCE_VOTING_REGISTRATION_INCONSISTENT_WITH_CIP36)
+            validate(params.votingPublicKeyHex == null || params.votingPublicKeyPath == null, InvalidDataReason.GOVERNANCE_VOTING_REGISTRATION_BOTH_KEY_AND_PATH)
+            validate(params.votingPublicKeyHex != null || params.votingPublicKeyPath != null, InvalidDataReason.GOVERNANCE_VOTING_REGISTRATION_MISSING_VOTING_KEY)
+        }
+        break
+
+    default:
+        throw new InvalidData(InvalidDataReason.GOVERNANCE_VOTING_DELEGATION_UNKNOWN_FORMAT)
+    }
+
+    const votingPublicKey = params.votingPublicKeyHex == null
+        ? null
+        : parseHexStringOfLength(params.votingPublicKeyHex, GOVERNANCE_VOTING_REGISTRATION_VOTING_VKEY_LENGTH, InvalidDataReason.GOVERNANCE_VOTING_REGISTRATION_INVALID_VOTING_KEY)
+
+    const votingPublicKeyPath = params.votingPublicKeyPath == null
+        ? null
+        : parseBIP32Path(params.votingPublicKeyPath, InvalidDataReason.GOVERNANCE_VOTING_REGISTRATION_INVALID_VOTING_KEY_PATH)
+
+    const delegations = params.delegations == null
+        ? null
+        : parseGovernanceVotingDelegations(params.delegations)
+
+    const votingPurpose = params.votingPurpose == null
+        ? null
+        : parseUint64_str(params.votingPurpose, {}, InvalidDataReason.GOVERNANCE_VOTING_REGISTRATION_INVALID_VOTING_PURPOSE)
 
     return {
-        type: TxAuxiliaryDataType.CATALYST_REGISTRATION,
-        votingPublicKey: parseHexStringOfLength(params.votingPublicKeyHex, CATALYST_REGISTRATION_VOTING_VKEY_LENGTH, InvalidDataReason.CATALYST_REGISTRATION_INVALID_VOTING_KEY),
-        stakingPath: parseBIP32Path(params.stakingPath, InvalidDataReason.CATALYST_REGISTRATION_INVALID_STAKING_KEY_PATH),
-        rewardsDestination: parseAddress(network, params.rewardsDestination) as ShelleyAddressParams,
-        nonce: parseUint64_str(params.nonce, {}, InvalidDataReason.CATALYST_REGISTRATION_INVALID_NONCE),
+        format: params.format,
+        votingPublicKey,
+        votingPublicKeyPath,
+        delegations,
+        stakingPath: parseBIP32Path(params.stakingPath, InvalidDataReason.GOVERNANCE_VOTING_REGISTRATION_INVALID_STAKING_KEY_PATH),
+        rewardsDestination: parseTxDestination(network, params.rewardsDestination, false),
+        nonce: parseUint64_str(params.nonce, {}, InvalidDataReason.GOVERNANCE_VOTING_REGISTRATION_INVALID_NONCE),
+        votingPurpose,
     }
 }