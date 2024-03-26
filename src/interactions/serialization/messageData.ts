vacuumlabs/ledgerjs-cardano-shelley

feature: message signing (CIP-8)

src/interactions/serialization/messageData.ts
@@ -1,0 +1,44 @@
+import type {Version} from '../../types/public'
+import {MessageAddressFieldType} from '../../types/public'
+import type {ParsedMessageData, Uint32_t, Uint8_t} from '../../types/internal'
+import {path_to_buf, uint32_to_buf, uint8_to_buf} from '../../utils/serialize'
+import {serializeAddressParams} from './addressParams'
+
+export function serializeMessageDataInit(
+  version: Version,
+  msgData: ParsedMessageData,
+): Buffer {
+  const msgLengthBuffer = uint32_to_buf(
+    (msgData.messageHex.length / 2) as Uint32_t,
+  )
+
+  const hashPayloadBuffer = msgData.hashPayload
+    ? uint8_to_buf(1 as Uint8_t)
+    : uint8_to_buf(0 as Uint8_t)
+
+  const isAsciiBuffer = msgData.isAscii
+    ? uint8_to_buf(1 as Uint8_t)
+    : uint8_to_buf(0 as Uint8_t)
+
+  const addressFieldTypeEncoding = {
+    [MessageAddressFieldType.ADDRESS]: 0x01,
+    [MessageAddressFieldType.KEYHASH]: 0x02,
+  } as const
+  const addressFieldTypeBuffer = uint8_to_buf(
+    addressFieldTypeEncoding[msgData.addressFieldType] as Uint8_t,
+  )
+
+  const addressBuffer =
+    msgData.addressFieldType === MessageAddressFieldType.ADDRESS
+      ? serializeAddressParams(msgData.address, version)
+      : Buffer.concat([])
+
+  return Buffer.concat([
+    msgLengthBuffer,
+    path_to_buf(msgData.signingPath),
+    hashPayloadBuffer,
+    isAsciiBuffer,
+    addressFieldTypeBuffer,
+    addressBuffer,
+  ])
+}

src/parsing/messageData.ts
@@ -1,0 +1,77 @@
+import {unreachable} from '../utils/assert'
+import {InvalidDataReason} from '../errors/invalidDataReason'
+import type {ParsedMessageData} from '../types/internal'
+import type {MessageData} from '../types/public'
+import {MessageAddressFieldType} from '../types/public'
+import {parseBIP32Path, parseHexString} from '../utils/parse'
+import {parseAddress} from './address'
+
+// check if a non-null-terminated buffer contains printable ASCII between 32 and 126 (inclusive)
+// copied from Ledger app
+function isPrintableAscii(buffer: Buffer): boolean {
+  for (let i = 0; i < buffer.length; i++) {
+    if (buffer[i] > 126) return false
+    if (buffer[i] < 32) return false
+  }
+
+  return true
+}
+
+// check if the string can be unambiguously displayed to the user
+// copied from Ledger app
+function isAscii(msg: string): boolean {
+  const buffer = Buffer.from(msg, 'hex')
+
+  // must not be empty
+  if (buffer.length === 0) return false
+
+  // no non-printable characters except spaces
+  if (!isPrintableAscii(buffer)) return false
+
+  const space = ' '.charCodeAt(0)
+
+  // no leading spaces
+  if (buffer[0] === space) return false
+
+  // no trailing spaces
+  if (buffer[buffer.length - 1] === space) return false
+
+  // only single spaces
+  for (let i = 0; i + 1 < buffer.length; i++) {
+    if (buffer[i] === space && buffer[i + 1] === space) return false
+  }
+
+  return true
+}
+
+export function parseMessageData(data: MessageData): ParsedMessageData {
+  const preferHexDisplay = data.preferHexDisplay || false
+  const common = {
+    signingPath: parseBIP32Path(
+      data.signingPath,
+      InvalidDataReason.MESSAGE_DATA_INVALID_WITNESS_PATH,
+    ),
+    isAscii: isAscii(data.messageHex) && !preferHexDisplay,
+    hashPayload: data.hashPayload,
+    messageHex: parseHexString(
+      data.messageHex,
+      InvalidDataReason.MESSAGE_DATA_INVALID_MESSAGE_HEX,
+    ),
+  }
+
+  switch (data.addressFieldType) {
+    case MessageAddressFieldType.ADDRESS:
+      return {
+        ...common,
+        addressFieldType: MessageAddressFieldType.ADDRESS,
+        address: parseAddress(data.network, data.address),
+      }
+    case MessageAddressFieldType.KEYHASH:
+      return {
+        ...common,
+        addressFieldType: MessageAddressFieldType.KEYHASH,
+      }
+    default:
+      unreachable(data)
+  }
+}

src/types/internal.ts
@@ -5,6 +5,7 @@ import {
   CIP36VoteDelegationType,
   CIP36VoteRegistrationFormat,
   DatumType,
+  MessageAddressFieldType,
   NativeScriptType,
   PoolKeyType,
   PoolOwnerType,
@@ -57,6 +58,7 @@ export {
 } from './public'
 // Our types
 export const EXTENDED_PUBLIC_KEY_LENGTH = 64
+export const PUBLIC_KEY_LENGTH = 32
 export const KEY_HASH_LENGTH = 28
 export const SCRIPT_HASH_LENGTH = 28
 export const TX_HASH_LENGTH = 32
@@ -602,6 +604,22 @@ export type ParsedOperationalCertificate = {
   issueCounter: Uint64_str
   coldKeyPath: ValidBIP32Path
 }
+
+export type ParsedMessageData =
+  | {
+      messageHex: HexString
+      signingPath: ValidBIP32Path
+      hashPayload: boolean
+      isAscii: boolean
+    } & (
+      | {
+          addressFieldType: MessageAddressFieldType.ADDRESS
+          address: ParsedAddressParams
+        }
+      | {
+          addressFieldType: MessageAddressFieldType.KEYHASH
+        }
+    )
 
 export type ParsedCVote = {
   voteCastDataHex: HexString

src/types/public.ts
@@ -294,6 +294,51 @@ export type OperationalCertificate = {
   coldKeyPath: BIP32Path
 }
 
+/**
+ * CIP-8 message signing
+ *
+ * The content of the `address` field in the protected header in Sig_structure in CIP-8
+ * is not defined. Some dApps use actual Cardano address, but in many cases it would be
+ * more useful to just use a key hash (e.g. for messages signed by DRep keys).
+ * This is implemented on Ledger.
+ */
+export enum MessageAddressFieldType {
+  ADDRESS = 'address',
+  KEYHASH = 'key_hash',
+}
+
+/**
+ * CIP-8 message signing
+ *
+ * @category Basic types
+ */
+export type MessageData = {
+  // message as bytes in hex
+  // Note: If the message is too long to be displayed at once, it must be hashed.
+  // MAX_CIP8_MSG_FIRST_CHUNK_ASCII_SIZE 198
+  // MAX_CIP8_MSG_FIRST_CHUNK_HEX_SIZE 99
+  messageHex: string
+
+  // the message will be signed with this key
+  signingPath: BIP32Path
+
+  // whether to hash the message when creating payload
+  hashPayload: boolean
+
+  // if true, message will be displayed in hex even if it is ascii
+  preferHexDisplay?: boolean
+} & (
+  | {
+      addressFieldType: MessageAddressFieldType.ADDRESS
+      address: DeviceOwnedAddress
+      network: Network
+    }
+  | {
+      addressFieldType: MessageAddressFieldType.KEYHASH
+      // the hash of key derived from witnessPath is used in the CIP-8 `address` field
+    }
+)
+
 /** CIP-36 vote
  *
  * @category Basic types
@@ -1234,6 +1279,10 @@ export type DeviceCompatibility = {
    * Whether we support Conway era transaction elements
    */
   supportsConway: boolean
+  /**
+   * Whether we support CIP-8 message signing
+   */
+  supportsMessageSigning: boolean
 }
 
 /**
@@ -1274,6 +1323,17 @@ export type ExtendedPublicKey = {
  */
 export type OperationalCertificateSignature = {
   signatureHex: string
+}
+
+/**
+ * CIP-8 message signature
+ * @category Basic types
+ * @see [[Ada.signMessage]]
+ */
+export type SignedMessageData = {
+  signatureHex: string
+  signingPublicKeyHex: string
+  addressFieldHex: string
 }
 
 /**

cardano-foundation/ledgerjs-hw-app-cardano

src/interactions/serialization/messageData.ts
@@ -1,0 +1,44 @@
+import type {Version} from '../../types/public'
+import {MessageAddressFieldType} from '../../types/public'
+import type {ParsedMessageData, Uint32_t, Uint8_t} from '../../types/internal'
+import {path_to_buf, uint32_to_buf, uint8_to_buf} from '../../utils/serialize'
+import {serializeAddressParams} from './addressParams'
+
+export function serializeMessageDataInit(
+  version: Version,
+  msgData: ParsedMessageData,
+): Buffer {
+  const msgLengthBuffer = uint32_to_buf(
+    (msgData.messageHex.length / 2) as Uint32_t,
+  )
+
+  const hashPayloadBuffer = msgData.hashPayload
+    ? uint8_to_buf(1 as Uint8_t)
+    : uint8_to_buf(0 as Uint8_t)
+
+  const isAsciiBuffer = msgData.isAscii
+    ? uint8_to_buf(1 as Uint8_t)
+    : uint8_to_buf(0 as Uint8_t)
+
+  const addressFieldTypeEncoding = {
+    [MessageAddressFieldType.ADDRESS]: 0x01,
+    [MessageAddressFieldType.KEY_HASH]: 0x02,
+  } as const
+  const addressFieldTypeBuffer = uint8_to_buf(
+    addressFieldTypeEncoding[msgData.addressFieldType] as Uint8_t,
+  )
+
+  const addressBuffer =
+    msgData.addressFieldType === MessageAddressFieldType.ADDRESS
+      ? serializeAddressParams(msgData.address, version)
+      : Buffer.concat([])
+
+  return Buffer.concat([
+    msgLengthBuffer,
+    path_to_buf(msgData.signingPath),
+    hashPayloadBuffer,
+    isAsciiBuffer,
+    addressFieldTypeBuffer,
+    addressBuffer,
+  ])
+}

src/parsing/messageData.ts
@@ -1,0 +1,77 @@
+import {unreachable} from '../utils/assert'
+import {InvalidDataReason} from '../errors/invalidDataReason'
+import type {ParsedMessageData} from '../types/internal'
+import type {MessageData} from '../types/public'
+import {MessageAddressFieldType} from '../types/public'
+import {parseBIP32Path, parseHexString} from '../utils/parse'
+import {parseAddress} from './address'
+
+// check if a non-null-terminated buffer contains printable ASCII between 32 and 126 (inclusive)
+// copied from Ledger app
+function isPrintableAscii(buffer: Buffer): boolean {
+  for (let i = 0; i < buffer.length; i++) {
+    if (buffer[i] > 126) return false
+    if (buffer[i] < 32) return false
+  }
+
+  return true
+}
+
+// check if the string can be unambiguously displayed to the user
+// copied from Ledger app
+function isAscii(msg: string): boolean {
+  const buffer = Buffer.from(msg, 'hex')
+
+  // must not be empty
+  if (buffer.length === 0) return false
+
+  // no non-printable characters except spaces
+  if (!isPrintableAscii(buffer)) return false
+
+  const space = ' '.charCodeAt(0)
+
+  // no leading spaces
+  if (buffer[0] === space) return false
+
+  // no trailing spaces
+  if (buffer[buffer.length - 1] === space) return false
+
+  // only single spaces
+  for (let i = 0; i + 1 < buffer.length; i++) {
+    if (buffer[i] === space && buffer[i + 1] === space) return false
+  }
+
+  return true
+}
+
+export function parseMessageData(data: MessageData): ParsedMessageData {
+  const preferHexDisplay = data.preferHexDisplay || false
+  const common = {
+    signingPath: parseBIP32Path(
+      data.signingPath,
+      InvalidDataReason.MESSAGE_DATA_INVALID_WITNESS_PATH,
+    ),
+    isAscii: isAscii(data.messageHex) && !preferHexDisplay,
+    hashPayload: data.hashPayload,
+    messageHex: parseHexString(
+      data.messageHex,
+      InvalidDataReason.MESSAGE_DATA_INVALID_MESSAGE_HEX,
+    ),
+  }
+
+  switch (data.addressFieldType) {
+    case MessageAddressFieldType.ADDRESS:
+      return {
+        ...common,
+        addressFieldType: MessageAddressFieldType.ADDRESS,
+        address: parseAddress(data.network, data.address),
+      }
+    case MessageAddressFieldType.KEY_HASH:
+      return {
+        ...common,
+        addressFieldType: MessageAddressFieldType.KEY_HASH,
+      }
+    default:
+      unreachable(data)
+  }
+}

src/types/internal.ts
@@ -5,6 +5,7 @@ import {
   CIP36VoteDelegationType,
   CIP36VoteRegistrationFormat,
   DatumType,
+  MessageAddressFieldType,
   NativeScriptType,
   PoolKeyType,
   PoolOwnerType,
@@ -57,6 +58,7 @@ export {
 } from './public'
 // Our types
 export const EXTENDED_PUBLIC_KEY_LENGTH = 64
+export const PUBLIC_KEY_LENGTH = 32
 export const KEY_HASH_LENGTH = 28
 export const SCRIPT_HASH_LENGTH = 28
 export const TX_HASH_LENGTH = 32
@@ -602,6 +604,22 @@ export type ParsedOperationalCertificate = {
   issueCounter: Uint64_str
   coldKeyPath: ValidBIP32Path
 }
+
+export type ParsedMessageData =
+  | {
+      messageHex: HexString
+      signingPath: ValidBIP32Path
+      hashPayload: boolean
+      isAscii: boolean
+    } & (
+      | {
+          addressFieldType: MessageAddressFieldType.ADDRESS
+          address: ParsedAddressParams
+        }
+      | {
+          addressFieldType: MessageAddressFieldType.KEY_HASH
+        }
+    )
 
 export type ParsedCVote = {
   voteCastDataHex: HexString

src/types/public.ts
@@ -294,6 +294,51 @@ export type OperationalCertificate = {
   coldKeyPath: BIP32Path
 }
 
+/**
+ * CIP-8 message signing
+ *
+ * The content of the `address` field in the protected header in Sig_structure in CIP-8
+ * is not defined. Some dApps use actual Cardano address, but in many cases it would be
+ * more useful to just use a key hash (e.g. for messages signed by DRep keys).
+ * This is implemented on Ledger.
+ */
+export enum MessageAddressFieldType {
+  ADDRESS = 'address',
+  KEY_HASH = 'key_hash',
+}
+
+/**
+ * CIP-8 message signing
+ *
+ * @category Basic types
+ */
+export type MessageData = {
+  // message as bytes in hex
+  // Note: If the message is too long to be displayed at once, it must be hashed.
+  // MAX_CIP8_MSG_FIRST_CHUNK_ASCII_SIZE 198
+  // MAX_CIP8_MSG_FIRST_CHUNK_HEX_SIZE 99
+  messageHex: string
+
+  // the message will be signed with this key
+  signingPath: BIP32Path
+
+  // whether to hash the message when creating payload
+  hashPayload: boolean
+
+  // if true, message will be displayed in hex even if it is ascii
+  preferHexDisplay?: boolean
+} & (
+  | {
+      addressFieldType: MessageAddressFieldType.ADDRESS
+      address: DeviceOwnedAddress
+      network: Network
+    }
+  | {
+      addressFieldType: MessageAddressFieldType.KEY_HASH
+      // the hash of key derived from witnessPath is used in the CIP-8 `address` field
+    }
+)
+
 /** CIP-36 vote
  *
  * @category Basic types
@@ -1234,6 +1279,10 @@ export type DeviceCompatibility = {
    * Whether we support Conway era transaction elements
    */
   supportsConway: boolean
+  /**
+   * Whether we support CIP-8 message signing
+   */
+  supportsMessageSigning: boolean
 }
 
 /**
@@ -1274,6 +1323,17 @@ export type ExtendedPublicKey = {
  */
 export type OperationalCertificateSignature = {
   signatureHex: string
+}
+
+/**
+ * CIP-8 message signature
+ * @category Basic types
+ * @see [[Ada.signMessage]]
+ */
+export type SignedMessageData = {
+  signatureHex: string
+  signingPublicKeyHex: string
+  addressFieldHex: string
 }
 
 /**