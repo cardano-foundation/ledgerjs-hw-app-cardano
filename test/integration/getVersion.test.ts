d77891b9f2ae7dea8151a8834ef81511bc3eb027
Jan Mazak <jan.mazak@vacuumlabs.com>
2026-03-23 01:21:57 +0100

    update app version in test

--- test/integration/getVersion.test.ts
+++ test/integration/getVersion.test.ts
@@ -21,7 +21,7 @@ describe('getVersion', () => {
     const {version, compatibility} = await ada.getVersion()
 
     expect(version.major).to.equal(7)
-    expect(version.minor).to.equal(1)
+    expect(version.minor).to.equal(3)
 
     expect(version.flags.isDebug).to.equal(true)
 
